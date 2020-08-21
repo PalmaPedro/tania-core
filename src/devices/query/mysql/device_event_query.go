4f2c52696ffc2322cf1a5a4ec8c5c5e71f60947a
PalmaPedro <pmcpalma@gmail.com>
2020-08-21 15:01:42 +0200

    started working on implementing 'devices' in the backend

--- src/devices/query/mysql/device_event_query.go
+++ src/devices/query/mysql/device_event_query.go
@@ -5,9 +5,13 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/PalmaPedro/tania-core/devices/query"
+
+	"github.com/PalmaPedro/tania-core/src/devices/decoder"
+	"github.com/PalmaPedro/tania-core/src/devices/query"
+	"github.com/PalmaPedro/tania-core/src/devices/storage"
 	uuid "github.com/satori/go.uuid"
 )
+
 // DeviceEventQueryMysql :
 type DeviceEventQueryMysql struct {
 	DB *sql.DB
@@ -17,4 +21,52 @@ type DeviceEventQueryMysql struct {
 func NewDeviceEventQueryMysql(db *sql.DB) query.DeviceEventQuery {
 	return &DeviceEventQueryMysql{DB: db}
 }
+
+// FindAllByDeviceID :
+func (f *DeviceEventQueryMysql) FindAllByDeviceID(uid uuid.UUID) <-chan query.QueryResult {
+	result := make(chan query.QueryResult)
+
+	go func() {
+		events := []storage.DeviceEvent{}
+
+		rows, err := f.DB.Query("SELECT * FROM DEVICE_EVENT WHERE DEVICE_UID = ? ORDER BY VERSION ASC", uid.Bytes())
+		if err != nil {
+			result <- query.QueryResult{Error: err}
+		}
+
+		rowsData := struct {
+			ID          int
+			TaskUID     []byte
+			Version     int
+			CreatedDate time.Time
+			Event       []byte
+		}{}
+
+		for rows.Next() {
+			rows.Scan(&rowsData.ID, &rowsData.DeviceUID, &rowsData.Version, &rowsData.CreatedDate, &rowsData.Event)
+
+			wrapper := decoder.DeviceEventWrapper{}
+			json.Unmarshal(rowsData.Event, &wrapper)
+
+			deviceUID, err := uuid.FromBytes(rowsData.DeviceUID)
+			if err != nil {
+				result <- query.QueryResult{Error: err}
+			}
+
+			events = append(events, storage.DeviceEvent{
+				DeviceUID:     deviceUID,
+				Version:     rowsData.Version,
+				CreatedDate: rowsData.CreatedDate,
+				Event:       wrapper.Data,
+			})
+		}
+
+		result <- query.QueryResult{Result: events}
+		close(result)
+	}()
+
+	return result
+}
+
+
 
